a1b75851f947e8f2935a5d2027db63744cda0f4b
Friedrich von Never <friedrich@fornever.me>
2019-03-10 15:09:46 +0700

    Add a plugin template

---
+++ src/dotnet/ReSharperPlugin.ForTea.Rider/SampleComponent.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Application;
+using JetBrains.ReSharper.Feature.Services.QuickFixes;
+using JetBrains.ReSharper.Intentions.CSharp.QuickFixes;
+
+namespace ReSharperPlugin.ForTea.Rider
+{
+    [ShellComponent]
+    internal class SampleQuickFixRegistrarComponent
+    {
+        public SampleQuickFixRegistrarComponent(IQuickFixes table)
+        {
+            table.RegisterQuickFix<SampleHighlighting>(null, h => new SampleFix(h.Declaration), typeof(SampleFix));
+        }
+    }
+}
