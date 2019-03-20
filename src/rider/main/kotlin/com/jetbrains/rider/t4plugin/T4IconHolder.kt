17dbbac6e56cd993efde409b2675bef2c40a1c34
Friedrich von Never <friedrich@fornever.me>
2019-03-20 22:45:25 +0700

    Fix the icon rendering

--- src/rider/main/kotlin/com/jetbrains/rider/t4plugin/T4IconHolder.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/t4plugin/T4IconHolder.kt
@@ -3,5 +3,5 @@ package com.jetbrains.rider.t4plugin
 import com.intellij.openapi.util.IconLoader
 
 object T4IconHolder {
-    val Icon = IconLoader.getIcon("ForTea.ico")
+    val Icon = IconLoader.getIcon("/icons/ForTea.png")
 }
